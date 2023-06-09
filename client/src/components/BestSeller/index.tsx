c8c0f9785ade242547823db55b1a4fe06876ed35
HarshShah08 <harsh.shah@codosphere.com>
2023-06-09 17:03:07 +0530

    ProductOverView card added

--- client/src/components/BestSeller/index.tsx
+++ client/src/components/BestSeller/index.tsx
@@ -1,13 +1,92 @@
 import React from "react";
 import ProductOverView from "../ProductOverView";
-
+const data = [
+  {
+    isNew: true,
+    image: "img/product/5.png",
+    quickView: "#",
+    addToCard: "#",
+    wishlist: "#",
+    stars: 5,
+    productName: "Antiseptic Spray",
+    price: "$32.00",
+    originalPrice: "$46.00",
+  },
+  {
+    isNew: true,
+    image: "img/product/5.png",
+    quickView: "#",
+    addToCard: "#",
+    wishlist: "#",
+    stars: 3,
+    productName: "Antiseptic Spray",
+    price: "$32.00",
+    originalPrice: "$46.00",
+  },
+  {
+    isNew: false,
+    image: "img/product/5.png",
+    quickView: "#",
+    addToCard: "#",
+    wishlist: "#",
+    stars: 4,
+    productName: "Antiseptic Spray",
+    price: "$32.00",
+    originalPrice: "$46.00",
+  },
+  {
+    isNew: true,
+    image: "img/product/5.png",
+    quickView: "#",
+    addToCard: "#",
+    wishlist: "#",
+    stars: 2,
+    productName: "Antiseptic Spray",
+    price: "$32.00",
+    originalPrice: "$46.00",
+  },
+  {
+    isNew: false,
+    image: "img/product/5.png",
+    quickView: "#",
+    addToCard: "#",
+    wishlist: "#",
+    stars: 3,
+    productName: "Antiseptic Spray",
+    price: "$32.00",
+    originalPrice: "$46.00",
+  },
+];
+interface ProductProps {
+  isNew: boolean;
+  image: string;
+  quickView: string;
+  addToCard: string;
+  wishlist: string;
+  stars: number;
+  productName: string;
+  price: string;
+  originalPrice: string;
+}
 export default function BestSeller() {
   return (
     <section>
-      <div className="max-w-6xl mx-auto">
-        <h1>Best Selling Item</h1>
-        <div className="grid grid-cols-4 ">
-          <ProductOverView />
+      <div className="max-w-6xl mx-auto py-14">
+        <h1 className="text-center mb-11 text-5xl">Best Selling Item</h1>
+        <div className="grid grid-cols-4 gap-5 ">
+          {data.map((item: ProductProps, index: number) => (
+            <ProductOverView
+              isNew={item.isNew}
+              image={item.image}
+              quickView={item.quickView}
+              addToCard={item.addToCard}
+              wishlist={item.wishlist}
+              stars={item.stars}
+              productName={item.productName}
+              price={item.price}
+              originalPrice={item.originalPrice}
+            />
+          ))}
         </div>
       </div>
     </section>
--- client/src/components/ProductOverView/index.tsx
+++ client/src/components/ProductOverView/index.tsx
@@ -1,73 +1,71 @@
-import React from "react";
+import { AiOutlineEye, AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
+import getStars from "../getStars";
 
-export default function ProductOverView() {
+interface ProductProps {
+  isNew: boolean;
+  image: string;
+  quickView: string;
+  addToCard: string;
+  wishlist: string;
+  stars: number;
+  productName: string;
+  price: string;
+  originalPrice: string;
+}
+export default function ProductOverView({
+  isNew = false,
+  image = "",
+  quickView = "#",
+  addToCard = "#",
+  wishlist = "#",
+  stars = 1,
+  productName = "",
+  price = "",
+  originalPrice = "",
+}: ProductProps) {
   return (
-    <div className="ltn__product-item ltn__product-item-3 text-left">
-      <div className="product-img">
+    <div className="ltn__product-item relative ltn__product-item-3 text-left hover:shadow-xl">
+      <div className="product-img relative">
         <a href="product-details.html">
-          <img src="img/product/10.png" alt="#" />
+          <img src={image} alt="#" />
         </a>
-        <div className="product-badge">
-          <ul>
-            <li className="sale-badge">New</li>
-          </ul>
-        </div>
+        {isNew && (
+          <div className="product-badge">
+            <ul>
+              <li className="sale-badge">New</li>
+            </ul>
+          </div>
+        )}
         <div className="product-hover-action">
           <ul>
             <li>
-              <a href="#" title="Quick View" data-bs-toggle="modal" data-bs-target="#quick_view_modal">
-                <i className="far fa-eye"></i>
+              <a href={quickView} title="Quick View" data-bs-toggle="modal" className="h-full" data-bs-target="#quick_view_modal">
+                <AiOutlineEye size={20} />
               </a>
             </li>
             <li>
-              <a href="#" title="Add to Cart" data-bs-toggle="modal" data-bs-target="#add_to_cart_modal">
-                <i className="fas fa-shopping-cart"></i>
+              <a href={addToCard} title="Add to Cart" data-bs-toggle="modal" data-bs-target="#add_to_cart_modal">
+                <AiOutlineShoppingCart size={20} />
               </a>
             </li>
             <li>
-              <a href="#" title="Wishlist" data-bs-toggle="modal" data-bs-target="#liton_wishlist_modal">
-                <i className="far fa-heart"></i>
+              <a href={wishlist} title="Wishlist" data-bs-toggle="modal" data-bs-target="#liton_wishlist_modal">
+                <AiOutlineHeart size={20} />
               </a>
             </li>
           </ul>
         </div>
       </div>
       <div className="product-info">
-        <div className="product-ratting">
-          <ul>
-            <li>
-              <a href="#">
-                <i className="fas fa-star"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fas fa-star"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fas fa-star"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fas fa-star-half-alt"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="far fa-star"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <h2 className="product-title">
-          <a href="product-details.html">Antiseptic Spray</a>
+        <div className="flex">{getStars({ stars })}</div>
+        <h2 className="product-title text-base">
+          <a href="product-details.html" className="text-primary hover:text-secondary">
+            {productName}
+          </a>
         </h2>
-        <div className="product-price">
-          <span>$32.00</span>
-          <del>$46.00</del>
+        <div className="product-price text-sm">
+          <span>{price}</span>
+          <del>{originalPrice}</del>
         </div>
       </div>
     </div>

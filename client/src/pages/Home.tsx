d0ab6826b30c47a759dbd9bb88e3648a76161599
Suhaas <suhaasvijay7@gmail.com>
2023-06-09 12:49:05 +0530

    feat: :art: Added carousel

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,10 +3,12 @@ import Banner from "../components/Home/Banner";
 import OfferBanner from "../components/Home/OfferBanner";
 import FeaturedProduct from "../components/Home/FeaturedProduct";
 import Stats from "../components/Stats";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div>
+      <Carousel />
       <FeaturedProduct />
       <BestSeller />
       <OfferBanner />

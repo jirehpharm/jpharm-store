61f0254aabc4ebb405313af6a07d6239d09779ab
sahana-sv <sahana.hv@codosphere.com>
2023-06-09 18:50:57 +0530

    changes done

--- client/src/components/Home/HeroSection.tsx
+++ client/src/components/Home/HeroSection.tsx
@@ -2,7 +2,7 @@ import { IHeroProps } from "./types";
 
 const HeroSection = ({ offer, title, price, buttonUrl }: IHeroProps) => {
   return (
-    <div className="max-w-7xl mx-auto my-14 grid grid-cols-5 text-center gap-7">
+    <div className="max-w-6xl mx-auto my-14 grid grid-cols-5 text-center gap-7">
       {/* <div className="col-span-3"> */}
       <div className="col-span-3 flex items-center bg-[url(placeholder/1408x993.jpg)]">
         <div className="ml-10 w-[40%] text-left">

eb0fe06258703ec153ca64f38f61f78159c8adda
sahana-sv <sahana.hv@codosphere.com>
2023-06-13 11:58:34 +0530

    feat: :sparkles: Created a product details card & Added a folder for PopularTags, ProductTab

--- client/src/components/AppContainer/index.tsx
+++ client/src/components/AppContainer/index.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home";
 import Register from "../../pages/Register";
 import Contact from "../../components/Contact/ContactCards";
-import BreadcrumbBanner from "../Common/BreadcrumbBanner";
+import ProductDeatils from "../../pages/ProductDeatils";
 
 export default function AppContainer() {
   return (
@@ -12,19 +12,7 @@ export default function AppContainer() {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/product-details"
-          element={
-            <>
-              <BreadcrumbBanner
-                title={"Antiseptic Spray"}
-                bgImage={
-                  "https://themexriver.com/wp/vicodin/wp-content/themes/vicodin/assets/images/bg/14.jpg"
-                }
-              />
-            </>
-          }
-        />
+        <Route path="/product-details" element={<ProductDeatils />} />
       </Route>
     </Routes>
   );

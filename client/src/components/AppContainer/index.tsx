a4ed163e6416701a60bae9d184fca40de8a02bce
Suhaas <suhaasvijay7@gmail.com>
2023-06-13 12:30:19 +0530

    Added Forgot Password page

--- client/src/components/AppContainer/index.tsx
+++ client/src/components/AppContainer/index.tsx
@@ -4,11 +4,13 @@ import Home from "../../pages/Home";
 import Register from "../../pages/Register";
 import Contact from "../../components/Contact/ContactCards";
 import BreadcrumbBanner from "../Common/BreadcrumbBanner";
+import ForgotPassword from "../../pages/ForgotPassword";
 
 export default function AppContainer() {
   return (
     <Routes>
       <Route>
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />

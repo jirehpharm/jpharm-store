70b4323022cba8c804835cfc5df270f17aefa424
sahana-sv <sahana.hv@codosphere.com>
2023-06-13 17:52:08 +0530

    feat: :sparkles: Added Product tab design

--- client/src/pages/ProductDetails.tsx
+++ client/src/pages/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import BreadcrumbBanner from "../components/Common/BreadcrumbBanner";
 import Details from "../components/Products/Details";
 import RelatedProducts from "../components/Products/RelatedProducts";
+import ProductTab from "../components/Products/ProductTab";
 
 export default function ProductDetails() {
   return (
@@ -9,6 +10,7 @@ export default function ProductDetails() {
       <div className="grid grid-cols-6 gap-5 mx-auto max-w-6xl my-20">
         <div className="col-span-4">
           <Details />
+          <ProductTab />
         </div>
         <div className="col-span-2">sidebar</div>
       </div>

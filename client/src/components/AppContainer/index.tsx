f266ff0a0cc9b9413705bcc704362cd620464217
sahana-sv <sahana.hv@codosphere.com>
2023-06-13 12:02:19 +0530

    Added Login Route

--- client/src/components/AppContainer/index.tsx
+++ client/src/components/AppContainer/index.tsx
@@ -4,12 +4,14 @@ import Home from "../../pages/Home";
 import Register from "../../pages/Register";
 import Contact from "../../components/Contact/ContactCards";
 import ProductDeatils from "../../pages/ProductDeatils";
+import Login from "../../pages/Login";
 
 export default function AppContainer() {
   return (
     <Routes>
       <Route>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product-details" element={<ProductDeatils />} />

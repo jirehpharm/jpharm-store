4c332eb1d1dc8650d5b4486156a8f35e19669a86
HarshShah08 <harsh.shah@codosphere.com>
2023-06-14 16:59:45 +0530

    feat: :art: search widget created
    search widget created

---
+++ client/src/components/Products/Search.tsx
@@ -0,0 +1,17 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+export default function Search({ setSearchText }: any) {
+  return (
+    <div className="flex items-center bg-sectionColor border-2 border-borderColor pl-5 pr-2">
+      <input
+        type="text"
+        placeholder="Search"
+        className="h-[60px] w-full bg-sectionColor font-semibold text-sm mb-0 focus-visible:outline-0 "
+        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+          setSearchText(e.currentTarget.value);
+        }}
+      />
+      <MagnifyingGlassIcon className="w-10 h-10 text-light" />
+    </div>
+  );
+}

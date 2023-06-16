a4dfdfdfb681bbbcd13e242e12a4ffa930fb7bbd
Suhaas <suhaasvijay7@gmail.com>
2023-06-16 16:59:12 +0530

    Adding offerBanner in about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -7,11 +7,14 @@ import CoreFeatureCard from "../components/Cards/CoreFeatureCard";
 import { coreFeatureData } from "../components/Cards/CoreFeatureCard/constants";
 import BreadcrumbBanner from "../components/Common/BreadcrumbBanner";
 import Stats from "../components/About/Stats";
+import { offerSectionData } from "../components/Home/constants";
+import OfferBanner from "../components/Home/OfferBanner";
 
 export default function About() {
   return (
     <div>
       <BreadcrumbBanner title={"About"} />
+      <OfferBanner {...offerSectionData} />
       <CoreFeatureCard data={coreFeatureData} />
       <Stats data={statsData}/>
       <QuestionsSection faqCategory={questionCategory}></QuestionsSection>

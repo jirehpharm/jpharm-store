f539436640ad98edafde13778e3677f02b221275
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-06-19 13:20:47 +0530

    style: :art: made contact cards reponsive

--- client/src/components/Blog/TrendingBlog.tsx
+++ client/src/components/Blog/TrendingBlog.tsx
@@ -12,11 +12,11 @@ export default function TrendingBlog({
   ));
 
   return (
-    <section className="pt-28 pb-[70px] mx-auto max-w-6xl">
+    <section className="pt-28 pb-[70px] mx-auto md:max-w-xl lg:max-w-6xl">
       <div className="relative px-4">
         <div className="relative mb-12 text-center">
-          <h6 className="font-semibold text-secondary">News & Blogs</h6>
-          <h1>Leatest News Feeds</h1>
+          <h6 className="font-semibold text-secondary text-xl">News & Blogs</h6>
+          <h1 className="text-2xl">Leatest Feeds</h1>
           <Carousel slidesPerView={3} slides={content} />
         </div>
       </div>

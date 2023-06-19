1028e1c7a7eed1293b8f7d30b3425f69f3e06c41
punithraj21 <punithraj.tcs21@gmail.com>
2023-06-19 19:21:15 +0530

    Added Registration backend functionality

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,40 +2,83 @@ import Button from "../components/Common/Button";
 import Layout from "../components/Common/Layout";
 import Checkbox from "../components/Form/Checkbox";
 import { InputField } from "../components/Form/InputField";
+import { gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+import { showErrorToast, showSuccessToast } from "../components/Toast/Toast";
 
 /**
  * Renders a registration form with input fields for first name, last name, email, password, and confirm password.
  *
  * @return {JSX.Element} The JSX element representing the register form
  */
+
+const fields = [
+  { name: "firstName", type: "text", placeholder: "First Name" },
+  { name: "lastName", type: "text", placeholder: "Last Name" },
+  { name: "email", type: "email", placeholder: "Email*", required: true },
+  {
+    name: "password",
+    type: "password",
+    placeholder: "Password*",
+    required: true,
+  },
+  {
+    name: "confirmPassword",
+    type: "password",
+    placeholder: "Confirm Password*",
+    required: true,
+  },
+];
+
+const REGISTER_CUSTOMER = gql`
+  mutation Mutation($email: String!, $password: String!, $fullName: String!) {
+    registerCustomer(email: $email, password: $password, fullName: $fullName) {
+      email
+    }
+  }
+`;
+
 export default function Register(): JSX.Element {
-  const fields = [
-    { name: "First Name", type: "text", placeholder: "First Name" },
-    { name: "Last Name", type: "text", placeholder: "Last Name" },
-    { name: "Email", type: "email", placeholder: "Email*", required: true },
-    {
-      name: "Password",
-      type: "password",
-      placeholder: "Password*",
-      required: true,
-    },
-    {
-      name: "Confirm Password",
-      type: "password",
-      placeholder: "Confirm Password*",
-      required: true,
-    },
-  ];
+  const [registerCustomer, { loading, error }] = useMutation(REGISTER_CUSTOMER);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const firstName = e.target.firstName.value;
+    const lastName = e.target.lastName.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+
+    if (email) {
+      const fullName = `${firstName} ${lastName}`;
+      const isValidPassword = password === confirmPassword;
+      if (!isValidPassword) {
+        showErrorToast("Passwords do not match");
+      } else {
+        const { data } = await registerCustomer({
+          variables: { email, password, fullName },
+        });
+        const isRegistered = data.registerCustomer;
+
+        if (isRegistered === null) {
+          showErrorToast("User already registered please login.");
+        }
+        if (error) {
+          showErrorToast("Something went wrong");
+        }
+        showSuccessToast("Registration successful");
+      }
+    }
+  };
 
   return (
     <Layout
       title={"Create an Account"}
-      subtitle={
-        "Sign up for our service to access exclusive content and features."
-      }
+      subtitle={"Sign up for our service to access exclusive content and features."}
     >
       <div className="sm:w-full lg:w-[470px]">
-        <form action="#">
+        <form action="#" onSubmit={handleSubmit}>
           {fields.map(({ name, type, placeholder, required }, index) => (
             <InputField
               key={index}
@@ -52,9 +95,8 @@ export default function Register(): JSX.Element {
             </div>
             <div className="ml-3 text-sm">
               <label htmlFor="consent" className="text-light text-sm">
-                I consent to Gerbil processing my personal data in order to send
-                personalized marketing material in accordance with the consent
-                form and the privacy policy.
+                I consent to Gerbil processing my personal data in order to send personalized
+                marketing material in accordance with the consent form and the privacy policy.
               </label>
             </div>
           </div>
@@ -69,15 +111,13 @@ export default function Register(): JSX.Element {
             </div>
           </div>
           <div className="flex justify-center">
-            <Button type="submit" variant="default">
+            <Button type="submit" variant="default" isLoading={loading}>
               CREATE ACCOUNT
             </Button>
           </div>
         </form>
         <div className="text-center text-sm text-grey-dark mt-4">
-          <p className="text-light">
-            By creating an account, you agree to our:
-          </p>
+          <p className="text-light">By creating an account, you agree to our:</p>
           <div className="flex space-x-4 justify-center">
             <a href="/terms-of-conditions">TERMS OF CONDITIONS</a>{" "}
             <span className="text-secondary">|</span>

bc12c9f597cdfdd948a948f238df16af564488db
punithraj21 <punithraj.tcs21@gmail.com>
2023-06-19 15:40:02 +0530

    Added react strict

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ApolloProvider, apolloClient } from "./apolloClient";
@@ -5,6 +6,8 @@ import "./style/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ApolloProvider client={apolloClient}>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </ApolloProvider>
 );

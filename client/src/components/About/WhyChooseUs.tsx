3dabe07738637e08d3796862672719f26d495933
Suhaas <suhaasvijay7@gmail.com>
2023-06-20 17:02:36 +0530

    style: :art: Made all the remaining responsive changes in about pages

--- client/src/components/About/WhyChooseUs.tsx
+++ client/src/components/About/WhyChooseUs.tsx
@@ -8,7 +8,7 @@ export default function WhyChooseUs(props: IWhyChooseUs) {
   const { data } = props;
   return (
     <div className=" bg-[#F2F6F7] py-20">
-      <h1 className="text-center pb-4 text-2xl sm:text-3xl lg:text-5xl">Why Choose Us<span className="text-2xl sm:text-3xl lg:text-5xl text-secondary">.</span></h1>
+      <h1 className="text-center pb-4 text-2xl sm:text-3xl lg:text-4xl">Why Choose Us<span className="text-2xl sm:text-3xl lg:text-4xl text-secondary">.</span></h1>
       <div className="max-w-[300px] md:max-w-2xl lg:max-w-[950px] xl:max-w-6xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-7">
         {data.map((item, index) => (
           <div key={index} className="p-8 border bg-white">

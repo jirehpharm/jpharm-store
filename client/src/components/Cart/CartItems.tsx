ccd3bcc7380bec25cc18192460f90091a25125ae
Suhaas <suhaasvijay7@gmail.com>
2023-06-20 17:46:16 +0530

    style: :art: Made cart page responsive

--- client/src/components/Cart/CartItems.tsx
+++ client/src/components/Cart/CartItems.tsx
@@ -8,15 +8,17 @@ interface ICart {
 export default function CartItems(props: ICart) {
   const { data } = props;
   return (
-    <div className="mx-auto mt-8 max-w-6xl">
+    <div className="mx-auto mt-8 max-w-[300px] sm:max-w-lg md:max-w-2xl lg:max-w-6xl">
       {data.map((item, index) => (
-        <div className="py-5 grid grid-cols-6 border-b-2" key={index}>
-          <div className="flex items-center justify-start">
+        <div className="py-5 grid grid-cols-1 md:grid-cols-6 border-b-2" key={index}>
+          <div className="flex items-center justify-center">
             <button className="w-20 h-10 " >x</button>
           </div>
-          <img src={item.image} className="h-[120px] w-[100px]" alt="Cart Items" />
-          <h5 className="mb-0 flex items-center justify-center">{item.title}</h5>
-          <p className="mb-0 flex items-center justify-center">{item.price}</p>
+          <div className="pt-3 flex justify-center items-center">
+            <img src={item.image} className="h-[120px] w-[100px]" alt="Cart Items" />
+          </div>
+          <h5 className="pt-3 mb-0 flex items-center justify-center text-base lg:text-lg">{item.title}</h5>
+          <p className="pt-3 mb-0 flex items-center justify-center text-sm lg:text-base">{item.price}</p>
           <div className="mb-0 flex items-center justify-center"><Counter /></div>
           <p className="mb-0 flex items-center justify-center">$889</p>
         </div >
--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -8,9 +8,9 @@ export default function Cart() {
   return (
     <>
       <BreadcrumbBanner title={"Cart"} />
-      <div className="my-24">
+      <div className="my-24 mx-5">
         <CartItems data={cartData} />
-        <div className="grid grid-cols-11 mx-auto mt-16 max-w-6xl">
+        <div className="grid grid-cols-1 lg:grid-cols-11 mx-auto mt-16 max-w-xs lg:max-w-6xl sm:max-w-xl">
           <div className="col-span-7"></div>
           <div className="col-span-4">
             <CartTotal data={cardTotalData} />

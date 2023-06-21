cfb733950b315b0bd904f4c378d84d5cbd66a57c
punithraj21 <punithraj.tcs21@gmail.com>
2023-06-21 16:00:42 +0530

    Added Data populate for database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from "express";
 
 import productsRoutes from "./modules/products/routes";
+import populateDataRoutes from "./modules/datafix/routes";
 
 export default function (): Router {
   const router: Router = Router();
 
   //Example Import
   router.use("/products", productsRoutes);
+  router.use("/datafix", populateDataRoutes);
 
   router.use("/healthCheck", (req: Request, res: Response) =>
     res.status(200).json({

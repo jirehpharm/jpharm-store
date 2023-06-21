18bf2287eb4eccfe50501e4088c1ec396421e32e
Abdul Wadudh <abdulwadudh5@gmail.com>
2023-06-21 06:43:52 +0530

    perf: :sparkles: Migrated Apollo V3 to V4 and Also using same port for Express and GraphQL

--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -1,5 +1,5 @@
 import knex from "knex";
 
-const pg = knex({ client: "pg", connection: process.env.PG_URL });
+const pg = knex({ client: "pg", connection: process.env.DATABASE_URL });
 
 export default pg;

35c970d06faa3f8cb75c84f5594f8f3dee0cbdcc
Suhaas <suhaasvijay7@gmail.com>
2023-06-21 11:16:26 +0530

    style: :art: Made all the remaining components in my-account and product-details page responsive

--- client/src/components/Common/Table/index.tsx
+++ client/src/components/Common/Table/index.tsx
@@ -1,7 +1,7 @@
 export default function Table({ thList = [], children }: { thList: string[]; children: React.ReactElement }) {
   return (
     <>
-      <div className="overflow-hidden border-t border-gray-200 ">
+      <div className="overflow-x-auto max-w-[290px] sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto border-t border-gray-200 ">
         <table className="min-w-full divide-y divide-gray-200 ">
           <thead className="bg-gray-50 ">
             <tr>

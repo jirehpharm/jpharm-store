74ff4ebd830394f766c0dfa933685e22e43c85df
Akilanpalani <akilanpalani2001@gmail.com>
2023-07-12 23:08:18 +0530

    Merge branch 'development' of github.com:jirehpharm/jpharm-store into product-detail

--- client/src/components/Tabs/HorizontalTab.tsx
+++ client/src/components/Tabs/HorizontalTab.tsx
@@ -9,16 +9,16 @@ interface TabProps {
 const HorizontalTab = (props: TabProps) => {
   const { tabsData } = props;
   return (
-    <div className="mx-2">
+    <div className="mx-6 bg-[#F2F5F7]">
       <Tabs.Root
         className="flex flex-col mt-4"
         defaultValue={get(tabsData, "0.tabTitle")}
       >
-        <Tabs.List className="border-b max-w-[300px] sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl overflow-x-auto border-gray-200 flex space-x-2 ">
+        <Tabs.List className="justify-between max-w-[300px] sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl overflow-x-auto flex space-x-2">
           {tabsData?.map((item, idx) => (
             <Tabs.Trigger
               key={idx}
-              className="p-3 text-sm md:text-base lg:text-lg data-[state=active]:text-secondary border-b-2 border-transparent data-[state=active]:border-b-2 focus-visible:outline-0 data-[state=active]:border-secondary font-bold"
+              className="text-sm md:text-base lg:text-lg data-[state=active]:text-secondary border-b-2 border-transparent data-[state=active]:border-b-2 focus-visible:outline-0 data-[state=active]:border-secondary font-bold"
               value={item.tabTitle}
             >
               {item.tabTitle}
@@ -28,12 +28,9 @@ const HorizontalTab = (props: TabProps) => {
         {tabsData?.map((item, idx) => (
           <Tabs.Content
             key={idx}
-            className="p-5 bg-white rounded-b-md outline-none text-light"
+            className="mt-10 p-10 bg-white rounded-b-md outline-none text-light"
             value={item.tabTitle}
           >
-            <h4 className="text-2xl lg:text-lg font-josefin font-medium">
-              <span className="text-secondary">|</span> {item.title}
-            </h4>
             {item.content}
           </Tabs.Content>
         ))}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Banner from "../components/Home/Banner";
 import OfferBanner from "../components/Home/OfferBanner";
 import FeaturedProduct from "../components/Home/FeaturedProduct";
 import Carousel from "../components/Carousel";
-import Stats from "../components/Common/Stats";
+
 import HeroSection from "../components/Home/HeroSection";
 import Category from "../components/Cards/Category/Category";
 import { categoryData } from "../components/Cards/Category/content";
@@ -14,11 +14,10 @@ import { featureProduct2, heroSectionData, offerCardData, offerSectionData, widg
 import TrendingProduct from "../components/Home/TrendingProduct";
 import FeaturedProduct2 from "../components/Home/FeaturedProduct2";
 import { blogDetails } from "../components/Blog/constant";
-import { statsData } from "../components/Common/Stats/constants";
+
 import { bestSellerData } from "../components/Home/constants";
 import OfferCard from "../components/Cards/OfferCard/OfferCard";
-import Bank from "../components/Common/BankAndDownload";
-import { bankData } from "../components/Common/BankAndDownload/constants";
+
 
 export default function Home() {
   const slideData = categoryData.map((item, idx: number) => <Category key={idx} Icon={item.Icon} title={item.title} />);
@@ -75,8 +74,7 @@ export default function Home() {
         bgImage={"/img/banner/N95.png"}
       />
       <TrendingBlog blogDetails={blogDetails} />
-      <Bank stats={bankData} />
-      <Stats stats={statsData} />
+      
     </div>
   );
 }

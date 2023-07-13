ec27cd2d8eeefa1d6bba4d2af4086628bfcbdd1e
HarshShah08 <harsh.shah@codosphere.com>
2023-07-13 16:14:30 +0530

    feat: :art: ptoducts page changes accouding to new design
    ptoducts page changes accouding to new design

--- client/src/components/Products/Search.tsx
+++ client/src/components/Products/Search.tsx
@@ -2,16 +2,18 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export default function Search({ setSearchText }: any) {
   return (
-    <div className="flex items-center bg-sectionColor border-2 border-borderColor pl-5 pr-2">
+    <div className="flex items-center ">
       <input
         type="text"
         placeholder="Search"
-        className="h-[60px] border-0 w-full  font-semibold text-sm mb-0 focus-visible:outline-0 bg-sectionColor "
+        className="  w-full h-full  font-semibold text-sm mb-0 focus:ring-0 focus:border-borderColor focus-visible:outline-0 bg-sectionColor border-2 border-borderColor "
         onChange={(e: React.FormEvent<HTMLInputElement>) => {
           setSearchText(e.currentTarget.value);
         }}
       />
-      <MagnifyingGlassIcon className="w-10 h-10 text-light" />
+      <div className="bg-secondary h-full flex items-center px-3 align-middle">
+        <MagnifyingGlassIcon className="w-8 h-8 text-sectionColor " />
+      </div>
     </div>
   );
 }
